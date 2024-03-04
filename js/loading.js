a8b8ac9ee2e089d26d8511b810bfba720c9431bf
Louis <p19970408@gmail.com>
2024-03-04 10:28:12 +0800

    updated

---
+++ js/loading.js
@@ -0,0 +1,19 @@
+function loading(tag) {
+  let number = $(tag).html();
+  let parse = parseInt(number);
+
+  let test;
+  let timeRun = setInterval(() => {
+    test = parse++;
+    $(tag).text(test);
+    if (test == 99) clearInterval(timeRun);
+  }, 50);
+}
+loading(".loading-progress");
+
+$(window).on("load", function () {
+  console.log("loading");
+  gsap.to(".loading-page", {
+    opacity: 0,
+  });
+});
